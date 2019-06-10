62de7a21b56d87b4324dbdf9edfc5bb45aaf340a
Levi-Huynh <Levi_huynh@yahoo.com>
2019-06-10 15:03:08 -0700

    used auth0 with nodejs doc

---
+++ data/helpers/users.js
@@ -0,0 +1,15 @@
+
+var express = require('express');
+var secured = require('../../mw');
+var router = express.Router();
+
+/* GET user profile. */
+router.get('/user', secured(), function (req, res, next) {
+  const { _raw, _json, ...userProfile } = req.user;
+  res.render('user', {
+    userProfile: JSON.stringify(userProfile, null, 2),
+    title: 'Profile page'
+  });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -5,12 +5,62 @@ const path = require('path');
 const actionRouter = require('./data/helpers/actionRouter.js');
 const projectRouter = require('./data/helpers/projectRouter.js');
 
+var userInViews = require('./userInViews.js');
+var authRouter = require('./data/helpers/authRouter.js');
+var usersRouter = require('./data/helpers/users.js');
+
+var session = require('express-session');
+var passport = require('passport');
+var Auth0Strategy = require('passport-auth0');
 
 const server = express();
 
 server.use(express.json());
 server.use(cors());
+
+
+
+var strategy = new Auth0Strategy(
+    {
+      domain: process.env.AUTH0_DOMAIN,
+      clientID: process.env.AUTH0_CLIENT_ID,
+      clientSecret: process.env.AUTH0_CLIENT_SECRET,
+      callbackURL:
+        process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback'
+    },
+    function (accessToken, refreshToken, extraParams, profile, done) {
+      // accessToken is the token to call Auth0 API (not needed in the most cases)
+      // extraParams.id_token has the JSON Web Token
+      // profile has all the information from the user
+      return done(null, profile);
+    }
+  );
+  
+  passport.use(strategy);
+  server.use(session(sess));
+server.use(passport.initialize());
+  server.use(passport.session());
+
+
+// ..
+server.use(userInViews());
+server.use('/', authRouter);
+server.use('/', usersRouter);
+
+// config express-session
+var sess = {
+  secret: 'CHANGE THIS TO A RANDOM SECRET',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true
+};
+
+if (server.get('env') === 'production') {
+  sess.cookie.secure = true; // serve secure cookies, requires https
+}
+
 server.use(express.static(path.join(__dirname, 'reactclient/build')));
+
 
 
 server.use('/api/action', actionRouter);
