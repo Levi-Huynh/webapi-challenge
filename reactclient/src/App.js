7b527e2eea8d1fda1ed922e7b2fd19e340b911ea
Levi-Huynh <Levi_huynh@yahoo.com>
2019-06-09 20:51:30 -0700

    imported login page tb auth0

--- reactclient/src/App.js
+++ reactclient/src/App.js
@@ -4,8 +4,8 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
 import PostsLists from './PostsList';
 import PostDetails from './PostDetails';
-import Nav from './Nav';
-
+// import Nav from './Nav';
+import Login from './Auth/login';
 import Auth from './Auth/Auth.js';
 import { Navbar, Button } from 'react-bootstrap';
 
@@ -80,7 +80,7 @@ class App extends Component {
         </Navbar>
       </div>
          <div className="mainbox">
-         <Route path='/' component={Nav}/>
+         {/* <Route path='/' component={Nav}/> */}
           <Route exact path='/login' component={Login}/>  
      <Route  path= '/' component={PostsLists}/>
      <Route exact path= '/projectdetails/:id' component={PostDetails}/>
